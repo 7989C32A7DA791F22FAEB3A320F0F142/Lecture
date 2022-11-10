b56701e636b16691b41f3bb6e8fe7da4db4b9357
lliliii <j.yh@kakao.com>
2022-11-10 20:11:35 +0900

    remove of not use module

--- 01_check_ip.py
+++ 01_check_ip.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-from datetime import datetime
 
 chrome_options = Options()
 chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
